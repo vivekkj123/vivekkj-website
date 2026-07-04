91d70816b56a538649d5de9b0b8e56d251da8bab
Vivek K J <vivekkj2004@gmail.com>
2026-07-04 18:58:36 +0530

    bugfix: navbar background was transparent on mobile in places other than hero sections

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
   return (
     <motion.div 
       className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${
-        scrolled ? "bg-black/50 backdrop-blur-lg border-b border-white/5" : "bg-transparent"
+        scrolled && !isMenuOpen ? "bg-black/50 backdrop-blur-lg border-b border-white/5" : "bg-transparent"
       }`}
       initial={{ y: -100 }}
       animate={{ y: 0 }}
